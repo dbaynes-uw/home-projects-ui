947a37faf03c1fada5ac3532e7d2f6ef1b781108
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2023-01-03 21:19:48 -0800

    -mFirst step to integrate tracking ui

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:3000/api/v1/areas/",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+export default {
+  getEvents() {
+    return apiClient.get("/");
+  },
+  getEvent(id) {
+    return apiClient.get("/" + id);
+  },
+};
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.description }}</h1>
+    <p>{{ event.duration }}</p>
+    <p>{{ event.frequency }} days</p>
+    <p>{{ event.notes }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from "@/services/EventService.js";
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+        console.log(error);
+      });
+  },
+};
+</script>
