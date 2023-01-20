5f6d4c9748d55ef53f5823546d9972819f56c3ca
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2023-01-19 17:37:29 -0800

    -mIcons for delete and back added

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,5 +1,11 @@
 <template>
-  <div v-if="event" class="event" id="center-align">
+  <div
+    v-if="event"
+    class="event"
+    id="center-align"
+    @dblclick="onDoubleClick(event)"
+    v-bind:class="{ 'is-complete': event.completed }"
+  >
     <h1>{{ event.description }}</h1>
     <ul class="ul-left">
       <li>Notes: {{ event.notes }}</li>
@@ -23,12 +29,32 @@
         </ul>
       </li>
     </ul>
+    <br />
+    <router-link :to="{ name: 'EventList' }">
+      <i class="fa-solid fa-backward fa-stack-1x"></i>
+    </router-link>
+    <span class="fa-stack">
+      <i
+        @click.prevent="removeEvent(event.id)"
+        class="fa-solid fa-pen-to-square fa-stack-1x"
+      >
+      </i>
+    </span>
+    <span class="fa-stack">
+      <i @click="removeEvent(event.id)" class="fas fa-trash-alt fa-stack-1x">
+      </i>
+    </span>
+    <br />
+    <router-link :to="{ name: 'EventList' }">
+      <i class="fa-solid fa-backward fa-stack-1x"></i>
+    </router-link>
   </div>
 </template>
 
 <script>
 import EventService from "@/services/EventService.js";
 import DateFormatService from "@/services/DateFormatService.js";
+import { mapActions } from "vuex";
 export default {
   props: ["id"],
   data() {
@@ -37,6 +63,25 @@ export default {
     };
   },
   methods: {
+    ...mapActions(["fetchEvents", "deleteEvent", "updateEvent"]),
+    removeEvent(id) {
+      this.deleteEvent(id);
+      alert("Event was successfully Deleted");
+      this.$router.push({ name: "EventList" });
+    },
+    onDoubleClick(currentEvent) {
+      const updatedEvent = {
+        id: currentEvent.id,
+        description: currentEvent.description,
+        notes: currentEvent.notes,
+        completed: !currentEvent.completed,
+        action_date: currentEvent.action_date,
+      };
+      //EventService.putEvent(updatedEvent);
+      this.updateEvent(updatedEvent);
+      console.log("Put Event executed: ", updatedEvent);
+      location.reload();
+    },
     formatStandardDate(value) {
       return DateFormatService.formatStandardDate(value);
     },
@@ -63,7 +108,10 @@ export default {
 };
 </script>
 <style scoped>
-#center-align {
+#align-right {
   text-align: center;
+}
+.right-align {
+  text-align: right;
 }
 </style>
