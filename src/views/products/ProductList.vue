a16d1c22a1d5a477c7535c58334c338d3efcfcc5
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2023-10-17 22:20:36 -0700

    -mFirst step in products

---
+++ src/views/products/ProductList.vue
@@ -0,0 +1,329 @@
+<template>
+  <v-card class="mx-auto mt-5">
+    <v-card-title class="pb-0">
+      <h3>Product List by Vendor</h3>
+    </v-card-title>
+  </v-card>
+  <v-card-text>
+    <v-form>
+      <v-container id="form-container">
+        <div class="row">
+          <div class="column">
+            <h2 @click='toggle1 = !toggle1'><b><u>Book Format1</u></b></h2>
+            <br/>
+            <div v-show='toggle1'>
+              <v-checkbox
+                label="Print"
+                v-model="book.format_print"
+              />
+              <v-checkbox
+                label="Audio"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test3"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test4"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test5"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test6"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test7"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test8"
+                v-model="book.format_audio"
+              />
+            </div>
+          </div>
+          <div class="column">
+            <h2 @click='toggle2 = !toggle2'><b><u>Book Format2</u></b></h2>
+            <br/>
+            <div v-show='toggle2'>
+              <v-checkbox
+                label="Print"
+                v-model="book.format_print"
+              />
+              <v-checkbox
+                label="Audio"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test3"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test4"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test5"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test6"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test7"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test8"
+                v-model="book.format_audio"
+              />
+            </div>
+          </div>
+          <div class="column">
+            <h2 @click='toggle3 = !toggle3'><b><u>Book Format3</u></b></h2>
+            <br/>
+            <div v-show='toggle3'>
+              <v-checkbox
+                label="Print"
+                v-model="book.format_print"
+              />
+              <v-checkbox
+                label="Audio"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test3"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test4"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test5"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test6"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test7"
+                v-model="book.format_audio"
+              />
+              <v-checkbox
+                label="Test8"
+                v-model="book.format_audio"
+              />
+            </div>
+          </div>
+        </div>
+        <span>
+          {{ urlMaxLength }} characters max -- {{ urlMaxLength - book.url_to_review.length }} characters remaining.
+        </span>
+        <v-text-field label="Url to Review"
+          v-model="book.url_to_review"
+          type="text"
+          :maxlength="urlMaxLength"
+          placeholder="URL to Review"
+        >
+          <template v-slot:prepend-inner>
+            <v-icon class="icon-css">mdi-link</v-icon>
+          </template>
+        </v-text-field>
+        <v-text-field label="Notes" v-model="book.notes">
+          <template v-slot:prepend-inner>
+            <v-icon class="icon-css">mdi-note</v-icon>
+          </template>
+        </v-text-field>
+        <v-btn type="submit" block class="mt-2" @click="onSubmit">Submit</v-btn>
+      </v-container>
+    </v-form>
+  </v-card-text>
+</template>
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  components: {
+  },
+  data() {
+    return {
+      book: {
+        title: null,
+        author: "",
+        date_written: null,
+        date_read: null,
+        format_audio: false,
+        format_print: false,
+        url_to_review: "",
+        notes: "",
+        created_by: "dbaynes",
+      },
+      toggle1: false,
+      toggle2: false,
+      toggle3: false,
+      isFormValid: false,
+      isAuthorValid: false,
+      isTitleValid: false,
+      urlMaxLength: 255,
+      num: 1,
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.checkValidations();
+      console.log("onSubmit - this.isFormValid: ", this.isFormValid)
+      if (this.isFormValid) {
+        const book = {
+          ...this.book,
+          id: uuidv4(),
+          created_by: this.$store.state.user,
+        };
+        if (this.$store.dispatch("createBook", book)) {
+          this.$router.push({ name: "BookList" });
+        } else {
+          alert("Error adding Book Location " + book.title);
+        }
+      } else {
+        alert("Please correct required fields and resubmit");
+      }
+    },
+    requiredAuthor: function (value) {
+      console.log("requiredAuthor: - this.isAuthorValid: ", this.isAuthorValid)
+      console.log("VALUE: ", value)
+      if (value) {
+          this.isAuthorValid = true
+          return true;
+      } else {
+          this.isFormValid = false
+          this.isAuthorValid = false
+          return 'Please enter Author';
+      }
+    },
+    requiredTitle: function (value) {
+      console.log("requiredTitle: - this.isTitleValid: ", this.isTitleValid)
+      console.log("VALUE: ", value)
+      if (value) {
+          this.isTitleValid = true
+          return true;
+      } else {
+          this.isFormValid = false
+          this.isTitleValid = false
+          return 'Please enter Title';
+      }
+    },
+    checkValidations() {
+      console.log("checkValidations - this.isFormValid: ", this.isFormValid)
+      console.log("checkValidations - this.isAuthorValid: ", this.isAuthorValid)
+      console.log("checkValidations - this.isTitleValid: ", this.isTitleValid)
+
+      if (this.isAuthorValid && this.isTitleValid) {
+        this.isFormValid = true
+      } else {
+        this.isFormValid = false
+      }
+      console.log("checkValidations Exit - this.isFormValid: ", this.isFormValid)
+      console.log("checkValidations Exit - this.isAuthorValid: ", this.isAuthorValid)
+      console.log("checkValidations Exit - this.isTitleValid: ", this.isTitleValid)
+
+    }
+  },
+  book() {
+    return this.$store.state.book;
+  },
+};
+</script>
+<style lang="css">
+
+/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33%;
+  padding: 10px;
+  /*height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.v-icon {
+  color: darkslategrey;
+  top: -0.2rem;
+}
+#form-container {
+  text-align: left;
+  width: 75% !important;
+}
+[type="checkbox"],
+#notes {
+  width: 100%;
+  height: 4rem;
+}
+.button {
+  margin: 30px;
+  background-color: #39495c;
+  border-radius: 5px;
+  font-size: 18px;
+  width: 160px;
+  height: 60px;
+  color: white;
+  padding: 20px;
+  box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
+    inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
+    inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
+  text-align: center;
+  cursor: pointer;
+}
+label {
+  font-size: 20px;
+  margin-bottom: 5px;
+}
+input {
+  width: 100%;
+  height: 40px;
+  margin-bottom: 20px;
+}
+fieldset {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+select {
+  border-color: darkgreen;
+}
+legend {
+  font-size: 28px;
+  font-weight: 700;
+  margin-top: 20px;
+}
+.label-visible {
+  top: -35px;
+  left: 4px;
+  visibility: visible;
+}
+.label-invisible {
+  top: -10px;
+  left: 4px;
+  visibility: hidden;
+}
+.input-field {
+  margin-top: 30px;
+  position: relative;
+}
+.input-field > input {
+  width: 100%;
+}
+.input-field > p {
+  position: absolute;
+  font-size: 14px;
+  transition: 0.3s;
+}
+</style>
