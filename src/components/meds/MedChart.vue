c8c45d324f8a8ad1c00a7189e93d883526b5a656
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2024-03-05 19:13:10 -0800

    -mRedeploy

--- src/components/meds/MedChart.vue
+++ src/components/meds/MedChart.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="med-chart">
-    <!--canvas id="medChart" width="400" height="400"></canvas-->
     <Bar :data="data" :options="options" />
   </div>
 </template>
 <script>
-
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import { Bar } from 'vue-chartjs';
 import DateFormatService from "@/services/DateFormatService.js";
