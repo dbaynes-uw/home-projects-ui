c13fcccf4fca23048045a0d1fe67d816c0522736
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2024-05-24 18:04:28 -0700

    -mDebut Main.js

--- src/main.js
+++ src/main.js
@@ -26,6 +26,7 @@ createApp({
     axios.interceptors.response.use(
       response => response,
       error => {
+        console.log("DEBUG: error: ", error)
         if (error.response.status === 401) {
           this.$store.dispatch('logout')
         }
