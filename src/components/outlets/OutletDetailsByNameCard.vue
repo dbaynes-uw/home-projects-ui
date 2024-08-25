93a8839cd81e8d1eb4fb8dc47b4d99ac0f39b27d
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2024-08-25 11:27:08 -0700

    -mProgress

--- src/components/outlets/OutletDetailsByNameCard.vue
+++ src/components/outlets/OutletDetailsByNameCard.vue
@@ -1,20 +1,20 @@
 <template>
-  OBN CARD - outlet_details_by_name? {{ outlet }}
-  <!--span v-if="outlet.active == 1"-->
+  <!--OBN CARD - outlet_details_by_name? {{ outlet }}-->
+  <span v-if="outlet.active == 1">
     <div class="card">
-      <p id="p-custom-left-u">@@{{ outlet.outlet_name }} </p> 
+      <p id="p-custom-left-u">{{ outlet.outlet_name }} </p> 
       <ul>
-        <!--li class="li-left">Status: <b>{{ outlet.active == 1 ? 'Active' : 'Inactive' }}</b></!--li-->
-        <!--li class="li-left">Line: <b>{{ outlet.line_number }}</b></!--li-->
+        <li class="li-left">Status: <b>{{ outlet.active == 1 ? 'Active' : 'Inactive' }}</b></li>
+        <li class="li-left">Line: <b>{{ outlet.line_number }}</b></li>
         <li class="li-left">Target: <b><u>{{ outlet.target }}</u></b></li>
         <li class="li-left">Frequency: {{ outlet.frequency }}</li>
         <li class="li-left">Start: {{ formatTime(outlet.start_time)}}</li>
         <li class="li-left">Duration: {{ outlet.duration }}</li>
         <li class="li-left">Notes: <b>{{ outlet.notes }}</b> </li>
-        <li class="li-left">{{ formatTime(outlet.date_of_occurrence) }} </li>
+        <!--li class="li-left">Time: {{ formatTime(outlet.date_of_occurrence) }} </!li-->
       </ul>
     </div>
-  <!--/~span-->
+  </span>
 </template>
 <script>
 
