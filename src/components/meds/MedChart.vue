4244199952c48c191ae78cd1f8e68ec405f71e21
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2024-10-22 17:10:59 -0700

    -mReintroducing Med Chart

--- src/components/meds/MedChart.vue
+++ src/components/meds/MedChart.vue
@@ -31,13 +31,14 @@ export default {
   },
   async mounted() {
     this.data.labels  = this.chartLabels
+    console.log("this.chartIntervals: ", this.chartIntervals)
     this.data.datasets.data = this.chartIntervals
     //for (let i=0; i < this.meds.length; i++) {
     //  this.data.labels[i] = DateFormatService.formatStandardDate(this.meds[i].date_of_occurrence)
     //  this.data.datasets[0].data[i] = this.meds[i].interval
     //  this.data.chartData[i] = DateFormatService.formatStandardDate(this.meds[i].date_of_occurrence)
     //}
-    //console.log("CHARTS DATA: ", this.data.datasets)
+    console.log("CHARTS DATA: ", this.data.datasets)
      // this.data.labels = ['Test4','Test5','Test6']
       //this.data.chartData = ['Test4','Test5','Test6']
   },
