4b653d18773342fee41c3c04d8aa7d38114d37e8
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2024-11-21 16:36:55 -0800

    -mTrying to break split on space

--- src/services/SplitStringService.js
+++ src/services/SplitStringService.js
@@ -3,6 +3,11 @@ export default {
     const result = []
     if (metaData != null) {
       for (let i = 0; i < metaData.length; i += splitLength) {
+        for (let t = 0; t < 10; t++) {
+          if (metaData.charAt(i + splitLength) != ' ') {
+            splitLength++
+          }
+        }
         result.push(metaData.slice(i, i + splitLength))
       }
     }
