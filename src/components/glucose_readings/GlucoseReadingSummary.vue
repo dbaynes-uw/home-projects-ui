025215523a96544814d777cd4ac6866bad40546e
David Baynes <davidbaynes@Davids-MBP.hsd1.wa.comcast.net>
2025-07-16 18:07:34 -0700

    -mGlucose Reading Status for 30, 60, 90 days

--- src/components/glucose_readings/GlucoseReadingSummary.vue
+++ src/components/glucose_readings/GlucoseReadingSummary.vue
@@ -77,26 +77,67 @@
       :style="{ color: isWithinRange(type, averageObj.average) }"
    >
       <li>
-        <p id="p-bold-indent">
-          - {{ type }} Average: {{ averageObj.average }} mg/dl ({{ averageObj.count }})
+        <p id="h3-left-subheading">
+          * {{ type }} Average: {{ averageObj.average }} mg/dl ({{ averageObj.count }})
         </p>
       </li>
     </ul>
     <br />
+    <h3 id="h3-left-subheading-green">
+      * AM-Fasting Normal (Last 60d): {{ amFastingNormal60 }}
+    </h3>
+    <h3 id="h3-left-subheading-blue">
+      * AM-Fasting Prediabetes (Last 60d): {{ amFastingPrediabetes60 }}
+    </h3>
+    <h3 id="h3-left-subheading-red">
+      * AM-Fasting Diabetes (Last 60d): {{ amFastingDiabetes60 }}
+    </h3>
+    <br />
+    <h3 id="h3-left-subheading-green">
+      * Post-Meal Normal (Last 60d): {{ postMealNormal60 }}
+    </h3>
+    <h3 id="h3-left-subheading-blue">
+      * Post-Meal Prediabetes (Last 60d): {{ postMealPrediabetes60 }}
+    </h3>
+    <h3 id="h3-left-subheading-red">
+      * Post-Meal Diabetes (Last 60d): {{ postMealDiabetes60 }}
+    </h3>
+    <br>
     <h3 id="h3-left-subheading">
       Averages Last 90 Days by Fasting Type (Total: {{ totalReadingsLast90days }})
     </h3>
+
     <ul
       v-for="(averageObj, type) in averageReadingsLast90daysByType"
       :key="type"
       :style="{ color: isWithinRange(type, averageObj.average) }"
     >
       <li>
-        <p id="p-bold-indent">
-          - {{ type }} Average: {{ averageObj.average }} mg/dl ({{ averageObj.count }})
+        <p id="h3-left-subheading">
+          * {{ type }} Average: {{ averageObj.average }} mg/dl ({{ averageObj.count }})
         </p>
       </li>
     </ul>
+    <h3 id="h3-left-subheading-green">
+      * AM-Fasting Normal (Last 90d): {{ amFastingNormal90 }}
+    </h3>
+    <h3 id="h3-left-subheading-blue">
+      * AM-Fasting Prediabetes (Last 90d): {{ amFastingPrediabetes90 }}
+    </h3>
+    <h3 id="h3-left-subheading-red">
+      * AM-Fasting Diabetes (Last 90d): {{ amFastingDiabetes90 }}
+    </h3>
+    <br />
+    <h3 id="h3-left-subheading-green">
+      * Post-Meal Normal (Last 90d): {{ postMealNormal90 }}
+    </h3>
+    <h3 id="h3-left-subheading-blue">
+      * Post-Meal Prediabetes (Last 90d): {{ postMealPrediabetes90 }}
+    </h3>
+    <h3 id="h3-left-subheading-red">
+      * Post-Meal Diabetes (Last 90d): {{ postMealDiabetes90 }}
+    </h3>
+
   </div>
 </template>
 
@@ -161,22 +202,33 @@ export default {
     const amFastingPrediabetes30 = computed(() => countAMFastingInRange(99, 126, 30));
     const amFastingDiabetes30 = computed(() => countAMFastingInRange(125, Infinity, 30));
     // Last 60 days
-    //const amFastingNormal60 = computed(() => countAMFastingInRange(70, 100, 60));
-    //const amFastingPrediabetes60 = computed(() => countAMFastingInRange(99, 126, 60));
-    //const amFastingDiabetes60 = computed(() => countAMFastingInRange(125, Infinity, 60));
+    const amFastingNormal60 = computed(() => countAMFastingInRange(70, 100, 60));
+    const amFastingPrediabetes60 = computed(() => countAMFastingInRange(99, 126, 60));
+    const amFastingDiabetes60 = computed(() => countAMFastingInRange(125, Infinity, 60));
 
-    //// Last 90 days
-    //const amFastingNormal90 = computed(() => countAMFastingInRange(70, 100, 90));
-    //const amFastingPrediabetes90 = computed(() => countAMFastingInRange(99, 126, 90));
-    //const amFastingDiabetes90 = computed(() => countAMFastingInRange(125, Infinity, 90));    
+    // Last 90 days
+    const amFastingNormal90 = computed(() => countAMFastingInRange(70, 100, 90));
+    const amFastingPrediabetes90 = computed(() => countAMFastingInRange(99, 126, 90));
+    const amFastingDiabetes90 = computed(() => countAMFastingInRange(125, Infinity, 90));    
     //
     //// Last 365 days
     //const amFastingNormal365 = computed(() => countAMFastingInRange(70, 100, 90));
     //const amFastingPrediabetes365 = computed(() => countAMFastingInRange(99, 126, 90));
     //const amFastingDiabetes365 = computed(() => countAMFastingInRange(125, Infinity, 90));    
-    const postMealNormal30 = computed(() => countPostMealInRange(70, 100, 30));
-    const postMealPrediabetes30 = computed(() => countPostMealInRange(100, 125, 30));
-    const postMealDiabetes30 = computed(() => countPostMealInRange(126, Infinity, 30));
+    
+    // Post-Meal Last 30 days
+    const postMealNormal30 = computed(() => countPostMealInRange(80, 140, 30));
+    const postMealPrediabetes30 = computed(() => countPostMealInRange(140, 200, 30));
+    const postMealDiabetes30 = computed(() => countPostMealInRange(201, Infinity, 30));
+    // Post-Meal Last 60 days
+    const postMealNormal60 = computed(() => countPostMealInRange(80, 140, 60));
+    const postMealPrediabetes60 = computed(() => countPostMealInRange(140, 200, 60));
+    const postMealDiabetes60 = computed(() => countPostMealInRange(201, Infinity, 60));
+    // Post-Meal Last 90 days
+    const postMealNormal90 = computed(() => countPostMealInRange(80, 140, 60));
+    const postMealPrediabetes90 = computed(() => countPostMealInRange(140, 200, 60));
+    const postMealDiabetes90 = computed(() => countPostMealInRange(201, Infinity, 60));
+
 
     const averageReadingsByType = computed(() => averageReadingsByDays(365)); // All readings
     const averageReadingsLast30daysByType = computed(() =>
@@ -301,15 +353,13 @@ export default {
       postMealNormal,
       postMealPrediabetes,
       postMealDiabetes,
-      amFastingNormal30,
-      amFastingPrediabetes30,
-      amFastingDiabetes30,
-      //amFastingNormal60, amFastingPrediabetes60, amFastingDiabetes60,
-      //amFastingNormal90, amFastingPrediabetes90, amFastingDiabetes90,
+      amFastingNormal30, amFastingPrediabetes30, amFastingDiabetes30,
+      amFastingNormal60, amFastingPrediabetes60, amFastingDiabetes60,
+      amFastingNormal90, amFastingPrediabetes90, amFastingDiabetes90,
       //amFastingNormal365, amFastingPrediabetes365, amFastingDiabetes365,
-      postMealNormal30,
-      postMealPrediabetes30,
-      postMealDiabetes30,
+      postMealNormal30, postMealPrediabetes30, postMealDiabetes30,
+      postMealNormal60, postMealPrediabetes60, postMealDiabetes60,
+      postMealNormal90, postMealPrediabetes90, postMealDiabetes90,
       isWithinRange,
     };
   },
